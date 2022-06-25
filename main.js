cdd917a626d56643927d812303f8c671d4368866
Diego Agudelo <agudelodiego22@gmail.com>
2022-06-25 17:48:29 -0500

    Corregi un bug, pero todo esta ray

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 let botonHabilidades = document.querySelector(".boton");
 let habilidades = document.querySelector(".habilidades");
 let porDefecto = document.querySelector(".porDefecto");
+let flag = 0;
+let rootStyle = document.documentElement.style;
 
 
 // Agregamos su correspondiente eventListener
@@ -14,3 +16,14 @@ botonHabilidades.addEventListener("click", (event)=>{
 });
 
 
+function forzarSlider(){
+    porDefecto.style.opacity = 1;
+    flag = 1;
+}
+
+function desForzar(){
+    if(flag == 1){
+        porDefecto.style.opacity = "";
+        flag = 2;
+    }
+}
